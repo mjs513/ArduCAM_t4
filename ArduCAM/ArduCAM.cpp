6c4ba92819f5a65f75238f6757d80c44467c1e8e
Mike S <CyberMerln@gmail.com>
2023-08-12 09:24:34 -0400

    Commented out define wire wire1 now in constructor

--- ArduCAM/ArduCAM.cpp
+++ ArduCAM/ArduCAM.cpp
@@ -112,9 +112,9 @@
 	#include <Wire.h>
 	#include <SPI.h>
 	#include "HardwareSerial.h"
-	#if defined(__SAM3X8E__)
-	#define Wire Wire1
-	#endif
+	//#if defined(__SAM3X8E__)
+	//#define Wire Wire1
+	//#endif
 #endif
 
 
